206683192710e2321f16fb8e0b6991cd8243c0a3
D1abloRUS <sergey.monakhov@gmail.com>
2017-12-18 23:36:46 +0300

    Update proxy_check.sh

--- proxy_check.sh
+++ proxy_check.sh
@@ -2,12 +2,9 @@
 
 while read _ _ _ PORT _ IP; do
   (PROXY=${IP}:$(echo ${PORT} | sed 's/\/tcp//')
-  STATUS=$(curl --max-time 2 -s -x ${PROXY} -o /dev/null -w '%{http_code}' https://m.vk.com)
-  if [ ${STATUS} -eq 200 ]; then
+  STATUS=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' http://${PROXY})
+  if [[ ${STATUS} =~ ^\{.*\}$ ]]; then
     echo GOOD: ${PROXY}
     echo "${PROXY}" >> good_list.txt
-  else
-    echo BAD: ${PROXY}
-    echo "${PROXY}" >> bad_list.txt
   fi) &
 done < <(tail -f /opt/proxylists.txt)
