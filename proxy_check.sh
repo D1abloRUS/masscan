9efc6a355ab2526dda5b3b54aa55d9e14a8bb346
D1abloRUS <sergey.monakhov@gmail.com>
2017-12-18 23:58:21 +0300

    Update proxy_check.sh

--- proxy_check.sh
+++ proxy_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 while read _ _ _ PORT _ IP; do
-  (PROXY=${IP}:$(echo ${PORT} | sed 's/\/tcp//')
-  STATUS=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' http://${PROXY})
+  PROXY=${IP}:$(echo ${PORT} | sed 's/\/tcp//')
+  STATUS=$(curl --max-time 5 -s -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}' http://${PROXY})
   if [[ ${STATUS} =~ ^\{.*\}$ ]]; then
     echo GOOD: ${PROXY}
     echo "${PROXY}" >> good_list.txt
-  fi) &
+  fi
 done < <(tail -f /opt/proxylists.txt)
